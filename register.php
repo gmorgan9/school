0555d789391d3de00746a08a57a24a560434ff68
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-09-05 17:13:10 -0600

    update

--- register.php
+++ register.php
@@ -56,7 +56,7 @@ if(isset($_POST['submit'])){
    <title>School | Registration</title>
 
    <!-- Custom Styles -->
-<link rel="stylesheet" href="<?php echo BASE_URL . '/assets/css/main-style.css?v='. time(); ?>">
+<link rel="stylesheet" href="<?php echo BASE_URL . '/assets/css/styles.css?v='. time(); ?>">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
