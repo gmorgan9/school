0ec8201cd47e0a08cfd5f238be8a3b24043c71e3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-09-05 17:08:24 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -4,7 +4,22 @@
             <a class="navbar-brand" href="/">
                 <h3><i class="bi bi-clock"></i> School</h3>
             </a>
+            <?php 
+
+                $uID = $_SESSION['uID'];
+                $select = " SELECT * FROM user WHERE userID = '$uID' ";
+                $result = mysqli_query($conn, $select);
+                if (mysqli_num_rows($result) > 0) {
+                   while($row = mysqli_fetch_assoc($result)) {
+                    $fname = $row['fname'];
+                    $loggedin = $row['loggedin'];
+                }}  
+
+            ?>
             <ul class="navbar-nav ms-auto d-flex flex-row">
+                <?php
+                    if($loggedin == 1) {
+                ?>
                 <li class="nav-item"><a class="nav-link me-3 me-lg-0">Welcome, <span style="text-transform: capitalize;"><?php echo $fname; ?></span>!</a></li>
                 <li class="nav-item"><a class="nav-link me-3 me-lg-0" href="<?php echo BASE_URL . '/admin/profile.php' ?>"><i class="bi bi-person"></i>  Profile</a></li>
                 
@@ -20,7 +35,13 @@
                     </ul>
                 </li>
                 <li class="nav-item"><a class="nav-link me-3 me-lg-0" href="<?php echo BASE_URL . '/logout.php' ?>">Logout</a></li>
+                <?php 
+                    } else {
+                ?>
                 <li class="nav-item"><a class="nav-link me-3 me-lg-0" href="<?php echo BASE_URL . '/login.php' ?>">Login/Signup</a></li>
+                <?php 
+                    }
+                ?>
             </ul>
         </div>
     </nav>
