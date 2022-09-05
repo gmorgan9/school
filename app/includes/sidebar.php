aee696a08b03434cd8920de4d65f4395b100f6c9
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-09-05 16:39:50 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -1,4 +1,4 @@
-<nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse">
+<nav id="sidebarMenu" class="sidebar collapse">
     <div>
         <div class="list-group list-group-flush mx-3 mt-4">
             <a href="<?php echo BASE_URL . '/pages/dashboard.php' ?>" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
