fff08386ec7ecb5b66fcb8166c861102979b8cdf
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-09-05 16:51:10 -0600

    update

--- app/includes/blocked-footer.php
+++
@@ -1,6 +0,0 @@
-<!-- WORKING -->
-<footer class="fixed-bottom py-3" style="background-color: white">
-        <div class="text-center text-muted" style="background-color: white;">
-        &copy; 2022 Workforce Management System
-    </div>
-</footer>
