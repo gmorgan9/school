32d7354e419da0053b92fecd7027fcb65f49253e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-09-05 16:25:40 -0600

    update

--- index.php
+++ index.php
@@ -1,8 +1,33 @@
-<?php 
+<!-- WORKING -->
+<?php
+require_once "../app/database/connection.php";
+require_once "../app/database/functions.php";
+require_once "../path.php";
+session_start();
 
-require_once "path.php";
+if(!isLoggedIn()){
+  header('location:' . BASE_URL . '/pages/entry/login.php');
+}
+
+// UPDATE JOB FUNCTION
+  if(isset($_POST['update-job'])){
+    $id = $_GET['jobID'];
+    $jobID = mysqli_real_escape_string($conn, $_POST['jobID']);
+    $idno  = rand(1000000, 9999999); // figure how to not allow duplicates
+    $jobtitle = mysqli_real_escape_string($conn, $_POST['jobtitle']);
+    $companyname = mysqli_real_escape_string($conn, $_POST['companyname']);
+    $deptname = mysqli_real_escape_string($conn, $_POST['deptname']);
+    $reason = mysqli_real_escape_string($conn, $_POST['reason']);
+  
+    $update = "UPDATE job SET approval_status = 'pending', jobtitle = '$jobtitle', companyname = '$companyname', deptname = '$deptname', reason = '$reason' WHERE jobID = '$id'";
+    mysqli_query($conn, $update);
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+  
+  };
+// END UPDATE JOB FUNCTION
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,29 +46,30 @@ require_once "path.php";
     <!-- scripts -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
-    <title>Home</title>
+    <title>WMS | Information</title>
 </head>
 <body>
     
+<?php include(ROOT_PATH . "/app/includes/header.php"); ?>
+<?php include(ROOT_PATH . "/app/includes/sidebar.php") ?>
   
 <!-- START MAIN -->
   <div class="main">
     <div class="page-header mx-auto">
-        <p class="page_title" style="float: left; padding-top: 2px;">Employee Information</p>
-        <ul class="breadcrumb">
-            <li><a href="<?php echo BASE_URL . '/pages/dashboard.php' ?>">Dashboard</a></li>
-            <li>Information</li>
-        </ul>
+      <p class="page_title" style="float: left; padding-top: 2px;">Employee Information</p>
+      <ul class="breadcrumb">
+        <li><a href="<?php echo BASE_URL . '/pages/dashboard.php' ?>">Dashboard</a></li>
+        <li>Information</li>
+      </ul>
     </div>
-
 
     <div class="page-content mx-auto mt-2">
-
     </div>
-  <!-- END ADMIN EMPLOYMENT INFO -->
-
   </div>
 <!-- END MAIN -->
+
+<?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
+
 
 </body>
 </html>
