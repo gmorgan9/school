fa824424c04f5917736e008c90946bacfe3ad5e4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-09-05 18:35:49 -0600

    update

--- login.php
+++ login.php
@@ -73,7 +73,7 @@ if(isLoggedIn()){
    
 
 
-<div class="circle1" style="background-color: #eee; height: 550px; width: 550px; position: absolute; border-radius: 350px; left: 150px; display:grid;"></div>
+<div class="circle1" style="background-color: #eee; height: 550px; width: 550px; position: absolute; border-radius: 350px; left: 150px; display:grid-inline;"></div>
 <br><br><br>
 
 <div class="form-container mx-auto">
